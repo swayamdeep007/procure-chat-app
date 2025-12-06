47919d94667cb415fda1d49c0594348a3d3ad9c7
Jay <jayvaghela806@gmail.com>
2025-12-06 11:02:29 +0530

    Add firebase config

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCYCfw2ySpZe8_xlufNmZZufvUkjSYCDeA",
+  authDomain: "procure-chat-app.firebaseapp.com",
+  projectId: "procure-chat-app",
+  storageBucket: "procure-chat-app.firebasestorage.app",
+  messagingSenderId: "212631169742",
+  appId: "1:212631169742:web:f66f0297eca3ea3dd63e9f"
+};
