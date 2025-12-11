c00c4cbb2ca2ebb682e5e87b2076c10c0a630ce2
Jay <jayvaghela806@gmail.com>
2025-12-11 11:53:23 +0530

    Admin Email Change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let CURRENT_USER_DESIGNATION = "";
 let selectedPLs = new Set();
 let isBulkMode = false;
 
-const ADMIN_EMAILS = ["jay@sbi.com", "ss@sbi.com"]; 
+const ADMIN_EMAILS = ["jay@sbi.com", "ss@sbi.com", "ravi@sbi.com", "shyam@sbi.com"]; 
 // A fast, local gray box placeholder to avoid external request delays
 const DEFAULT_AVATAR = "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22100%22%20height%3D%22100%22%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22%23e0e0e0%22%2F%3E%3Ctext%20x%3D%2250%25%22%20y%3D%2250%25%22%20font-family%3D%22sans-serif%22%20font-size%3D%2235%22%20fill%3D%22%23888%22%20dy%3D%22.3em%22%20text-anchor%3D%22middle%22%3EPL%3C%2Ftext%3E%3C%2Fsvg%3E";
 
