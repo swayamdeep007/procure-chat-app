68a2c3584dc6d06700e3ffee7bba1a180ebc5353
Jay <jayvaghela806@gmail.com>
2025-12-11 12:51:34 +0530

    Export firebaseConfig named export

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyA63bSlHOa5kyq4welhG3jRKIL8j-xFBpk",
   authDomain: "procure-chat-app-d75f7.firebaseapp.com",
   projectId: "procure-chat-app-d75f7",
